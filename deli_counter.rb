9072a9445fb5f94981355cb3c0520270233693f8
William T Cao <williamacao@gmail.com>
2020-08-28 18:54:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,4 @@ def line(katz_deli)
   katz_deli.length == 0
   puts "The line is currently empty."
 else
-  puts "The line is currently:"
-end
-def take_a_number(katz_deli, name)
-end
+  puts "Welcome, "#{name}". You are number "#{katz_deli}" in line."
