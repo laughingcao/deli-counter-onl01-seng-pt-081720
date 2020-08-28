c0da2e734a2583da1ab0acdd439ff4672c07ccbd
William T Cao <williamacao@gmail.com>
2020-08-28 18:58:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 katz_deli = []
-
+counter = 0
 def line(katz_deli)
   katz_deli.length == 0
   puts "The line is currently empty."
+  counter += 1
 else
   puts "Welcome, "#{name}". You are number "#{katz_deli}" in line."
