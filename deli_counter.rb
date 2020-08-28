f256506da5e4641330076e3144f0af0af6cbaa21
William T Cao <williamacao@gmail.com>
2020-08-28 19:02:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,6 @@ def line(katz_deli)
   katz_deli.length == 0
   puts "The line is currently empty."
 else
+  when katz_deli <= 1
   puts "Welcome, "#{name}". You are number "#{katz_deli}" in line."
 end
